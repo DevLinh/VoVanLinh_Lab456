54bcaec55f71c046b9334181cab76fec46728b40
devlinh <devlinh99qb@gmail,com>
2020-06-17 22:50:22 +0700

    hoàn thành api ở 6.9

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using Lab456.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab456.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendence = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _dbContext.Attendances.Add(attendence);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
